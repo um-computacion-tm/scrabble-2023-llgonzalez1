50f8f82cb569ef70c86946e3d4e51e6d82eb0d47
lucas gonzalez <llgonzalez@alumno.um.edu.ar>
2023-09-06 11:45:01 -0300

    corrigo cell

--- scrabble/cell.py
+++ scrabble/cell.py
@@ -1,18 +1,33 @@
 from scrabble.models import Tile
 
 class Cell:
-    def __init__(self, letter, multiplier=1, multiplier_type= None):
-        self.letter = letter
+    def __init__(self, multiplier, multiplier_type, letter, active):
         self.multiplier = multiplier
         self.multiplier_type = multiplier_type
-              
+        self.letter = letter
+        self.active = active
+
     def add_letter(self, letter:Tile):
         self.letter = letter
 
     def calculate_value(self):
         if self.letter is None:
             return 0
-        if self.multiplier_type == 'letter':
+        if self.multiplier_type == 'L' and self.active == True:
             return self.letter.value * self.multiplier
         else:
             return self.letter.value
+
+class calculate_word_value:
+    def __init__(self,word):
+        self.word = word
+        
+    def calculate_word(self):
+        values = 0
+        for cell in self.word:
+            values += cell.calculate_value()
+        for cell in self.word:
+            if cell.multiplier_type == 'W' and cell.active == True:
+                values = values * cell.multiplier
+
+        return values
