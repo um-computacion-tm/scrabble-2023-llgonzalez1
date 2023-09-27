ec89e9609fb8e5f68b7ed9ecbd88a3b77dd076e2
lucas gonzalez <llgonzalez@alumno.um.edu.ar>
2023-09-27 08:54:27 -0300

    corrijo calculate word

--- scrabble/calculate_word_value.py
+++ scrabble/calculate_word_value.py
@@ -1,9 +1,16 @@
 from scrabble.cell import Cell
 from scrabble.models import Tile
-class calulate_word_value:
-    
-    def calculate_word_value(word):
-        total_value = 0
-        for cell in word:
-            total_value += Cell.calculate_value()
-        return total_value
+
+class calculate_word_value:
+    def __init__(self,word):
+        self.word = word
+        
+    def calculate_word(self):
+        values = 0
+        for cell in self.word:
+            values += Cell.calculate_value()
+        for cell in self.word:
+            if Cell.multiplier_type == 'W' and Cell.active == True:
+                values = values * Cell.multiplier
+
+        return values
