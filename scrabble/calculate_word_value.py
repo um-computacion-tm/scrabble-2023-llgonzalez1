d78a03e71113ca888bb4818ec6264b1076ea0b41
lucas gonzalez <llgonzalez@alumno.um.edu.ar>
2023-09-27 08:51:22 -0300

    corrijo calculate word value

--- scrabble/calculate_word_value.py
+++ scrabble/calculate_word_value.py
@@ -5,5 +5,5 @@ class calulate_word_value:
     def calculate_word_value(word):
         total_value = 0
         for cell in word:
-            total_value += cell.calculate_value()
+            total_value += Cell.calculate_value()
         return total_value
